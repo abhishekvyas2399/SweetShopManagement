2f0863d482bc0485792628ed83b26aa104edd0a7
abhishekvyas2399 <abhishekvyas2399@gmail.com>
2025-09-30 22:36:35 +0530

    refactor: solved the bug of latency due to salt
    salt was high(20) which taking too much time to calculate around 1 minute so reduce it to salt=10 for less round and time

--- backend/src/service/auth.service.ts
+++ backend/src/service/auth.service.ts
@@ -7,7 +7,7 @@ const JWT_SECRET=process.env.JWT_SECRET as string;
 
 // register user in DB after hash his password then create a token as well so user get access of app just after register and without login.
 export const registerUserService=async (name:string,email:string,password:string,role:'CUSTOMER')=>{
-    const hashedPassword=await bcrypt.hash(password,20);
+    const hashedPassword=await bcrypt.hash(password,10);
     const user=await prisma.user.create({data:{name,email,password:hashedPassword,role}});
     const token=jwt.sign({userId:user.id},JWT_SECRET,{expiresIn: "3h"});
     return {user,token};
